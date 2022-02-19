349b0bbb8d07656a8095f62937baa51816d73cdd
Mahdi Qiamast <78082316+Qiamast@users.noreply.github.com>
2022-02-19 18:50:30 +0330

    some fix

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# @Qiamast
 import logging
 import sys
 import time
@@ -6,31 +5,47 @@ import time
 import telebot
 from telebot import types
 
-API_TOKEN = 'i dont love to paste my bot token here for public repo :)'
+API_TOKEN = 'Paste Bot Token Here!'
 
 bot = telebot.TeleBot(API_TOKEN)
 telebot.logger.setLevel(logging.DEBUG)
 
 
-@bot.inline_handler(lambda query: query.query == 'help')
+@bot.inline_handler(lambda query: query.query == 'text')
 def query_text(inline_query):
     try:
-        r = types.InlineQueryResultArticle('1', 'نتیجه اولی که با api کال نشده', types.InputTextMessageContent('رفیق هنوز زوده واسه کار'))
-        r2 = types.InlineQueryResultArticle('2', 'نتیجه دوم', types.InputTextMessageContent('سلام مثل اینکه حالت خوش نیست گزینه دوم انتخاب کردی'))
+        r = types.InlineQueryResultArticle('1', 'Result1', types.InputTextMessageContent('hi'))
+        r2 = types.InlineQueryResultArticle('2', 'Result2', types.InputTextMessageContent('hi'))
         bot.answer_inline_query(inline_query.id, [r, r2])
     except Exception as e:
         print(e)
 
 
-@bot.inline_handler(lambda query: query.query == 'pic')
+@bot.inline_handler(lambda query: query.query == 'lana')
 def query_photo(inline_query):
     try:
         r = types.InlineQueryResultPhoto('1',
-                                         'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Lana_Rhoades_cropped.jpg/820px-Lana_Rhoades_cropped.jpg',
+                                         'https://raw.githubusercontent.com/eternnoir/pyTelegramBotAPI/master/examples/detailed_example/kitten.jpg',
+                                         'https://raw.githubusercontent.com/eternnoir/pyTelegramBotAPI/master/examples/detailed_example/kitten.jpg',
                                          input_message_content=types.InputTextMessageContent('hi'))
         r2 = types.InlineQueryResultPhoto('2',
-                                          'https://i.pinimg.com/736x/62/a8/a3/62a8a32e09822d647e12387b8f5d907c.jpg')
+                                          'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Lana_Rhoades_cropped.jpg/820px-Lana_Rhoades_cropped.jpg',
+                                          'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Lana_Rhoades_cropped.jpg/820px-Lana_Rhoades_cropped.jpg')
         bot.answer_inline_query(inline_query.id, [r, r2], cache_time=1)
+    except Exception as e:
+        print(e)
+
+
+@bot.inline_handler(lambda query: query.query == 'video')
+def query_video(inline_query):
+    try:
+        r = types.InlineQueryResultVideo('1',
+                                         'https://github.com/eternnoir/pyTelegramBotAPI/blob/master/tests/test_data/test_video.mp4?raw=true',
+                                         'video/mp4', 'Video',
+                                         'https://raw.githubusercontent.com/eternnoir/pyTelegramBotAPI/master/examples/detailed_example/rooster.jpg',
+                                         'Title'
+                                         )
+        bot.answer_inline_query(inline_query.id, [r])
     except Exception as e:
         print(e)
 
@@ -38,14 +53,14 @@ def query_photo(inline_query):
 @bot.inline_handler(lambda query: len(query.query) == 0)
 def default_query(inline_query):
     try:
-        r = types.InlineQueryResultArticle('1', 'I am developing :) ', types.InputTextMessageContent('Hey my name is Imarch :.:.:'))
+        r = types.InlineQueryResultArticle('1', 'Now , I am developing :), I think Mahdi is writing code.', types.InputTextMessageContent('Now , I am developing :) @MahdiQiamast'))
         bot.answer_inline_query(inline_query.id, [r])
     except Exception as e:
         print(e)
 
 
 def main_loop():
-    bot.polling(True)
+    bot.infinity_polling()
     while 1:
         time.sleep(3)
 
